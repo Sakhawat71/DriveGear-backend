94f14e41a83bc805b87f64e3ce28d4d6364c38eb
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-25 02:45:44 +0600

    get singel product data from data base

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,6 +32,12 @@ async function run() {
         app.get('/products', async(req,res)=>{
             const cursor = dirveGearProducts.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.get('/products/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await dirveGearProducts.findOne(query);
             res.send(result)
         })
 
