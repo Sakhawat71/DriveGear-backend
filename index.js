aa025935787d2da360bdb9f6bc0ebb45f5bd9db1
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-25 17:51:01 +0600

    fixed vercel live link -> remove 'await' from 'client.connect();'

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const dirveGearProducts = client.db('dirveGear').collection('products');
 
