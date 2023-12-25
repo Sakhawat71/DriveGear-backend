af9c72bf81dcac0822cef62ce65a8efcc0c4fd0b
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-25 17:04:40 +0600

    update vercel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json())
 
 
